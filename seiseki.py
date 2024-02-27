76fa4c58e45e51acfc580202c87a0151ba360ff6
newmu74 <146675279+nemenemeneme@users.noreply.github.com>
2024-02-27 18:17:44 +0900

    Update seiseki.py
    例外処理を追加

--- seiseki.py
+++ seiseki.py
@@ -1,31 +1,41 @@
 import csv
 import glob 
+import sys
 
 pattern = "SIRS*"
 files = glob.glob(pattern)
 
+if not files:
+    print("ファイルが見つかりません")
+    sys.exit(1)
+
 grade_sum = 0
 tanni = 0
-aRatio = 0
-for file in files:
-    with open (file)as file:
+a_ratio = 0
+for file_name in files:
+    with open (file_name)as file:
         reader = csv.reader(file)
         for row in reader:
             if(row[7]=='A+'):
-                grade = 4+0.3
-                aRatio += 1*float(row[4])
+                grade = 4.3
+                a_ratio += 1*float(row[4])
             elif(row[7]=='A'):
-                grade = 3+1
-                aRatio += 1*float(row[4])
+                grade = 4
+                a_ratio += 1*float(row[4])
             elif(row[7]=='B'):
-                grade = 2+1
+                grade = 3
             elif(row[7]=='C'):
-                grade = 1+1
+                grade = 2
             elif(row[7]=='D'):
                 grade = 0
             else:
                 continue
             grade_sum += grade * float(row[4])
             tanni += float(row[4])
-        aRatio /= tanni
-    print(f'ファイル名:{file.name}, GPA:{grade_sum / tanni}, Aの割合:{aRatio}')
+        if(tanni != 0): 
+            a_ratio /= tanni
+            gpa = grade_sum / tanni
+        else:
+            a_ratio = 0
+            gpa = 0
+    print(f'ファイル名:{file.name}, GPA:{gpa}, Aの割合:{a_ratio}')
